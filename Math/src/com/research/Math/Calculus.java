22206a03620bb20c772932c5dc229dc7ce688e6e
000407 <romeoalfa407@gmail.com>
2016-09-28 15:13:32 +0530

    Calculus.java:+++

---
+++ Math/src/com/research/Math/Calculus.java
@@ -0,0 +1,10 @@
+package com.research.Math;
+
+/**
+ * Created by 000407 on 9/28/2016.
+ */
+public class Calculus{
+	public static void perform(){
+		System.out.println("Performing calculus!");
+	}
+}
