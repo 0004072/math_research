6bd4a125cbeb5f72ae1117c178c56ca30a8bf975
0004072 <kanchana@hsenidlanka.com>
2016-09-29 15:55:20 +0530

    divide::development underway!

--- Math/src/com/research/Math/Operators.java
+++ Math/src/com/research/Math/Operators.java
@@ -1,56 +1,87 @@
 package com.research.Math;
-import java.math.BigInteger;
-import java.util.regex.*;
 
 /**
  * Created by 000407 on 9/23/2016.
  */
 public class Operators {
-    private static int[] num1, num2, ans;
+    private static int[] num1, num2;
 
     public static void main(String[] args) {
-        //perform("1024", "1024", "*");
-        //perform("11a22", "2937", "=");
-        //perform("123401651621651651651616541564542616516516556416516516516516516216541651651651651651651651651655454", "3128678165118941561651616515165165162165165165165165165165165156515616516516516516516516516516516511", "*");
-        System.out.println(lt("594651465", "494644654646551864561564"));
-        /*gt("2345", "2456");//false
-        gt("2345", "3456");//false
-        System.out.println();
-        gt("2345", "2345");//true
-        gt("2345", "2344");//true
-        gt("2345", "2334");//true
-        gt("2345", "2234");//true
-        gt("2345", "1234");//true*/
+        System.out.println(toStr(divide("38", "7", 5)));
+    }
+
+    //Transformation methods: Converts a string into an array
+
+    /**
+     * Transforms a given number as a string, into an integer array in the reverse order.
+     *
+     * @param num Number to be transformed as a java.lang.String
+     * @param len Length of the required array
+     * @return Returns the integer array, is the transformation is successful, null otherwise.
+     * @throws Error if the string is not really an integer value. (containing invalid characters in a number).
+     */
+    private static int[] transformReverse(String num, int len) throws Error {
+        int arr[] = null;
+        if (!num.matches("^[+-]?\\d+$"))
+            throw new Error("Invalid number detected!");
+        arr = new int[len];
+        for (int i = 0; i < num.length(); i++) {
+            arr[num.length() - i - 1] = num.codePointAt(i) - 48;
+        }
+        return arr;
+    }
+
+    /**
+     * Transforms a given number as a string, into an integer array.
+     *
+     * @param num Number to be transformed as a java.lang.String
+     * @param len Length of the required array
+     * @return Returns the integer array, is the transformation is successful, null otherwise.
+     * @throws Error if the string is not really an integer value. (containing invalid characters in a number).
+     */
+    private static int[] transform(String num, int len) throws Error {
+        int arr[] = null;
+        if (!num.matches("^[+-]?\\d+$"))
+            throw new Error("Invalid number detected!");
+        arr = new int[len];
+        for (int i = 0; i < num.length(); i++) {
+            arr[i] = num.codePointAt(i);
+        }
+        return arr;
     }
 
     /**
      * Generates the diminished radix complement of a decimal number.
+     *
      * @param arr Array containing the integer to find the complement of
      */
-    private static void ninesComp(int[] arr){
-        for(int i=0;  i<arr.length-1; i++)
-            arr[i] = 9-arr[i];
+    private static void ninesComp(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++)
+            arr[i] = 9 - arr[i];
     }
 
     /**
      * Generates the radix complement of a decimal number.
+     *
      * @param arr Number that needs to be complemented.
      */
-    private static void tensComp(int[] arr){
-        for(int i=0;  i<arr.length-1; i++)
-            arr[i] = 9-arr[i];
-        int[] one = new int[ans.length];
+    private static int[] tensComp(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++)
+            arr[i] = 9 - arr[i];
+        int[] one = new int[arr.length];
         one[0] = 1;
         positionAdd(arr, one, arr, 0, 0);
+        return arr;
     }
 
     /**
      * Compares two given integers. Integers are taken as strings.
+     *
      * @param num1 Number to compare
      * @param num2 Number to be compared with
      * @return int[] array containing the indices of the
-     *      - first occurrence of value at index in num1 is less than the value at index in num2.
-     *      - first occurrence of value at index in num1 is greater than the value at index in num2.
+     * - first occurrence of value at index in num1 is less than the value at index in num2.
+     * - first occurrence of value at index in num1 is greater than the value at index in num2.
      */
     private static int[] compare(String num1, String num2) {
         boolean flags[][];
@@ -60,26 +91,26 @@ public class Operators {
 
         for (int i = 0; i < len; i++) {
             if (i < num1.length())
-                n1[i] = num1.codePointAt(num1.length()-i-1) - 48;
+                n1[i] = num1.codePointAt(num1.length() - i - 1) - 48;
 
             if (i < num2.length())
-                n2[i] = num2.codePointAt(num2.length()-i-1) - 48;
+                n2[i] = num2.codePointAt(num2.length() - i - 1) - 48;
         }
 
-        for(int i = 0; i < len; i++){
+        for (int i = 0; i < len; i++) {
             flags[len - i - 1][0] = n1[i] >= n2[i];
 
             flags[len - i - 1][1] = n1[i] <= n2[i];
         }
 
-        int flag[] = {0,0};
-        for(flag[0]=0; flag[0]<flags.length; flag[0]++){
-            if(!flags[flag[0]][0])
+        int flag[] = {0, 0};
+        for (flag[0] = 0; flag[0] < flags.length; flag[0]++) {
+            if (!flags[flag[0]][0])
                 break;
         }
 
-        for(flag[1]=0; flag[1]<flags.length; flag[1]++){
-            if(!flags[flag[1]][1])
+        for (flag[1] = 0; flag[1] < flags.length; flag[1]++) {
+            if (!flags[flag[1]][1])
                 break;
         }
 
@@ -88,28 +119,30 @@ public class Operators {
 
     /**
      * Performs the positional addition recursively until reaches the end of the array.
-     * @param a First addend
-     * @param b Second addend
+     *
+     * @param a   First addend
+     * @param b   Second addend
      * @param ans Sum
      * @param pos Current position being operated
-     * @param c Carry forward to next position
+     * @param c   Carry forward to next position
      */
-    private static void positionAdd(int[] a, int[] b, int[] ans, int pos, int c){
-        if(pos == ans.length-1)
+    private static void positionAdd(int[] a, int[] b, int[] ans, int pos, int c) {
+        if (pos == ans.length)
             return;
-        int numA = pos<a.length?a[pos]:0, numB = pos<b.length?b[pos]:0;
-        ans[pos] = (numA+numB+c)%10;
-        int carry = (numA+numB+c)/10;
+        int numA = pos < a.length ? a[pos] : 0, numB = pos < b.length ? b[pos] : 0;
+        ans[pos] = (numA + numB + c) % 10;
+        int carry = (numA + numB + c) / 10;
         positionAdd(a, b, ans, ++pos, carry);
     }
 
     /**
      * Performs the subtraction at a given position.
-     * @param a The minuend
-     * @param b The subtrahend
+     *
+     * @param a   The minuend
+     * @param b   The subtrahend
      * @param ans The difference
      * @param pos Current position being operated
-     * @param br Borrow from the next position
+     * @param br  Borrow from the next position
      */
     private static void positionSubtract(int[] a, int[] b, int[] ans, int pos, int br) {
         if (pos == ans.length)
@@ -128,183 +161,240 @@ public class Operators {
 
     /**
      * Performs the multiplication over an indicated position recursively.
-     * @param num The multiplicand
-     * @param mp The multiplier
+     *
+     * @param num    The multiplicand
+     * @param mp     The multiplier
      * @param posAns Array that holds the multiple
-     * @param pos Current position being operated
-     * @param c Carry forward to the next point
+     * @param pos    Current position being operated
+     * @param c      Carry forward to the next point
      * @param offset Offset from the decimal point before adding to the overall answer
      */
-    private static void positionMultiply(int[] num, int mp, int[] posAns, int pos, int c, int offset){
-        if(pos == num.length)
+    private static void positionMultiply(int[] num, int mp, int[] posAns, int pos, int c, int offset) {
+        if (pos == num.length)
             return;
 
-        int val = (num[pos]*mp+c)%10;
-        c = (num[pos]*mp+c)/10;
-        posAns[pos+offset] = val;
+        int val = (num[pos] * mp + c) % 10;
+        c = (num[pos] * mp + c) / 10;
+        posAns[pos + offset] = val;
 
         positionMultiply(num, mp, posAns, ++pos, c, offset);
     }
 
-    /**
-     * Performs the multiplication over 2 given numbers. Generates the result into an array in the reverse order.
-     */
-    private static void multiply(){
-        ans = new int[num1.length+num2.length];
-        for(int i=0; i<num2.length; i++){
-            int[] cur = new int[num1.length+i+1];
-            positionMultiply(num1, num2[i], cur, 0, 0, i);
-            positionAdd(ans, cur, ans, 0, 0);
+    private static void positionDivide(int n1[], int n2[], int ans[], int i) {
+        if (i == ans.length)
+            return;
+        int cyc[] = new int[ans.length];
+        int tmp[] = new int[cyc.length];
+        int j = 0;
+        while (true) {
+            for (int x = 0; x < cyc.length; x++) {
+                tmp[x] = cyc[x];
+            }
+
+            positionAdd(tmp, n2, tmp, 0, 0);
+            boolean flag = gt(toStr(tmp), toStr(n1));
+            if (flag) {
+                break;
+            }
+
+            for (int x = 0; x < cyc.length; x++) {
+                cyc[x] = tmp[x];
+            }
+
+            j++;
         }
-    }
-
-    private static void divide(String n1, String n2){
-        // TODO: 9/28/16 implement transformation algorithm
-        int ans[] = new int[n1.length()];
-
-        while(true){
-
+        // TODO: 9/29/16 Leads to errors when dealing division of very large numbers with small numbers.
+        ans[i] = j;
+        positionSubtract(n1, cyc, n1, 0, 0);
+        for (int x = n1.length - 1; x > 0; x--) {
+            n1[x] = n1[x - 1];
+            n1[x - 1] = 0;
         }
+        positionDivide(n1, n2, ans, ++i);
     }
 
     //comparison methods
 
     /**
      * Compares two given numbers.
+     *
      * @param n1 Number to compare
      * @param n2 Number to compare with
      * @return Return true if the first is greater than the second, false otherwise.
      */
-    private static boolean gt(String n1, String n2){
+    private static boolean gt(String n1, String n2) {
         int arr[] = compare(n1, n2);
-        return (arr[0]>arr[1]);
+        return (arr[0] > arr[1]);
     }
 
     /**
      * Compares two given numbers.
+     *
      * @param n1 Number to compare
      * @param n2 Number to compare with
      * @return Return true if the first is greater than or equal to the second, false otherwise.
      */
-    private static boolean ge(String n1, String n2){
+    private static boolean ge(String n1, String n2) {
         int arr[] = compare(n1, n2);
         return (arr[0] >= arr[1]);
     }
 
     /**
      * Compares two given numbers.
+     *
      * @param n1 Number to compare
      * @param n2 Number to compare with
      * @return Return true if the first is less than the second, false otherwise.
      */
-    private static boolean lt(String n1, String n2){
+    private static boolean lt(String n1, String n2) {
         int arr[] = compare(n1, n2);
-        return (arr[0]<arr[1]);
+        return (arr[0] < arr[1]);
     }
 
     /**
      * Compares two given numbers.
+     *
      * @param n1 Number to compare
      * @param n2 Number to compare with
      * @return Return true if the first is less than or equal to the second, false otherwise.
      */
-    private static boolean le(String n1, String n2){
+    private static boolean le(String n1, String n2) {
         int arr[] = compare(n1, n2);
         return (arr[0] <= arr[1]);
     }
 
     /**
      * Compares two given numbers.
+     *
      * @param n1 Number to compare
      * @param n2 Number to compare with
      * @return Return true if the first is equal to the second, false otherwise.
      */
-    private static boolean eq(String n1, String n2){
+    private static boolean eq(String n1, String n2) {
         int arr[] = compare(n1, n2);
         return (arr[0] == arr[1]);
     }
 
     /**
      * Compares two given numbers.
+     *
      * @param n1 Number to compare
      * @param n2 Number to compare with
      * @return Return true if the first is not equal to the second, false otherwise.
      */
-    private static boolean ne(String n1, String n2){
+    private static boolean ne(String n1, String n2) {
         int arr[] = compare(n1, n2);
         return (arr[0] != arr[1]);
     }
 
+    //Arithmetic operations
+
     /**
-     * Performs the indicated operation over the given numbers. If the inputs are invalid or the operator is invaid,
-     * an error will be thrown.
-     * @param a First operand for the operation.
-     * @param b Second operand for the operation.
-     * @param op Operation to perform.
+     * Adds two given integers
+     *
+     * @param a Addend
+     * @param b Addend
+     * @return integer array containing the sum of the addends in the reverse order
      */
-    public static void perform(String a, String b, String op){
-        int i = a.length(), j = b.length();
-        int len = i > j ? i: j;
+    public static int[] add(String a, String b) {
+        int i = a.length(), j = b.length(), ans[] = null;
+        int len = i > j ? i : j;
         ++len;
-
-        num1 = new int[len];
-        num2 = new int[len];
-        ans = new int[len];
-
-        try{
-            if(!op.matches("[+*/-]{1}"))
-                throw new Error(String.format("Invalid operation detected! %s", op));
-            int val;
-            for(int k=0; k<i; k++){
-                val = a.codePointAt(i-k-1)-48;
-                if(val < 0 || val > 9)
-                    throw new Error(String.format("Invalid number('%c') detected!", a.charAt(k)));
-                num1[k] = val;
-            }
-
-            for(int k=0; k<j; k++){
-                val = b.codePointAt(j-k-1)-48;
-                if(val < 0 || val > 9)
-                    throw new Error(String.format("Invalid number('%c') detected!", b.charAt(k)));
-                num2[k] = val;
-            }
-
-            switch(op){
-                case "+":
-                    positionAdd(num1, num2, ans, 0, 0);
-                    break;
-
-                case "-":
-                    positionSubtract(num1, num2, ans, 0, 0);
-                    if(ans[ans.length-1] == 9) {
-                        tensComp(ans);
-                    }
-                    break;
-
-                case "*":
-                    multiply();
-                    break;
-            }
-            populateAns();
-        }
-        catch(Throwable th){
+        try {
+            num1 = transformReverse(a, len);
+            num2 = transformReverse(b, len);
+            ans = new int[len];
+            positionAdd(num1, num2, ans, 0, 0);
+        } catch (Throwable th) {
             th.printStackTrace();
         }
+        return ans;
     }
 
     /**
-     * Populate ans[] array into a string in a human readable format.
+     * Performs the multiplication of two given numbers.
+     *
+     * @param n1 Multiplicand
+     * @param n2 Multiplier
+     * @return Returns the product of the two, as a reversed int[] array
      */
-    public static void populateAns() {
-        String str = "";
-        for(int k=0; k<ans.length; k++) {
-            int z = ans.length-k-1;
-            if(k==0 && ans[z]==9)
-                str += "-";
-            else
-                str += ans[z];
+    private static int[] multiply(String n1, String n2) {
+        int ans[] = null;
+        try {
+            int len = n1.length() + n2.length();
+            num1 = transformReverse(n1, len);
+            num2 = transformReverse(n2, len);
+            ans = new int[len];
+            for (int i = 0; i < num2.length; i++) {
+                int[] cur = new int[num1.length + i + 1];
+                positionMultiply(num1, num2[i], cur, 0, 0, i);
+                positionAdd(ans, cur, ans, 0, 0);
+            }
+        } catch (Error er) {
+            er.printStackTrace();
+        }
+        return ans;
+    }
+
+    public static int[] subtract(String a, String b) {
+        int i = a.length(), j = b.length(), ans[] = null;
+        int len = i > j ? i : j;
+        ++len;
+        try {
+            num1 = transformReverse(a, len);
+            num2 = transformReverse(b, len);
+            ans = new int[len];
+            positionSubtract(num1, num2, ans, 0, 0);
+        } catch (Throwable th) {
+            th.printStackTrace();
+        }
+        return ans;
+    }
+
+    private static int[] divide(String n1, String n2, int pre) {
+        int ans[] = new int[pre];
+        try {
+            num1 = transformReverse(n1, pre);
+            num2 = transformReverse(n2, pre);
+            positionDivide(num1, num2, ans, 0);
+        } catch (Error er) {
+            er.printStackTrace();
+        }
+        return ans;
+    }
+
+    /**
+     * Populates the given answer to a human readable format.
+     *
+     * @param arr Integer array to be transformed.
+     */
+    public static void populateAns(String op1, String op2, String op, int arr[]) {
+        if (arr == null) {
+            System.out.println("Empty array!");
+            return;
         }
 
-        System.out.println("Answer="+str);
+        String str = "";
+        for (int k = 0; k < arr.length; k++) {
+            int z = arr.length - k - 1;
+            if (k == 0 && arr[z] == 9)
+                str += "-";
+            else
+                str += arr[z];
+        }
+
+        System.out.printf("%s %s %s = %s\n", op1, op, op2, str);
+    }
+
+    private static String toStr(int arr[]) {
+        if (arr == null) {
+            System.out.println("Empty array!");
+            return null;
+        }
+        String s = "";
+        for (int i : arr)
+            s = String.format("%d%s", i, s);
+
+        return s;
     }
 }
